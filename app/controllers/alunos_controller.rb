fb26afa63b5bd0982b66cb2ad4a2b2a9f858b4fc
Breno Nogueira <bcnogueira@id.uff.br>
2018-10-31 17:02:29 -0300

    Adiciona create para aluno.

--- app/controllers/alunos_controller.rb
+++ app/controllers/alunos_controller.rb
@@ -6,4 +6,19 @@ class AlunosController < ApplicationController
   def show
     @aluno = Aluno.find(params[:id])
   end
+
+  def new
+    @aluno = Aluno.new
+  end
+
+  def create
+    @aluno = Aluno.create(aluno_params)
+    redirect_to aluno_path(@aluno)
+  end
+
+  private
+
+  def aluno_params
+    params.require(:aluno).permit(:nome, :iduff, :matricula, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # get 'home/index'
 
-  resources :alunos, only: [:index, :show]
+  resources :alunos, only: [:index, :show, :new, :create]
   resources :disciplinas, only: [:index]
   resources :professores, only: [:index, :show]
   resources :turmas, only: [:index, :show]
