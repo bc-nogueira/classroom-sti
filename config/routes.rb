f418e7489c4a05562304843028e4320927c114c1
Breno Nogueira <bcnogueira@id.uff.br>
2018-10-31 16:39:12 -0300

    Adiciona show de aluno.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # get 'home/index'
 
-  resources :alunos, only: [:index]
+  resources :alunos, only: [:index, :show]
   resources :disciplinas, only: [:index]
   resources :professores, only: [:index]
   resources :turmas, only: [:index, :show]
